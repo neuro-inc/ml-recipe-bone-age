a1950f9ab89489d12d5e8c9abca28f1db79bf016
Yevhenii Semendiak <semendyak@gmail.com>
2021-02-02 15:33:50 +0200

    minor fixes

--- label_studio/launch_ls.py
+++ label_studio/launch_ls.py
@@ -109,7 +109,7 @@ def _save_results(target_csv_file: Path, source: io.BytesIO):
     source_csv = pd.read_csv(source)
     source_transformed = source_csv.apply(_convert_row, axis=1)
     source_transformed = source_transformed.astype(target_csv.dtypes)
-    source_transformed.index = source_transformed.pop("id")
+    source_transformed = source_transformed.set_index("id")
     merged = pd.concat([target_csv, source_transformed])
     merged.to_csv(target_csv_file, index=False)
 
